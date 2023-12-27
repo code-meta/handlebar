b7b2502f119c9156a0f228039f16e37190eb2a22
fahim <fahim.x@yahoo.com>
2023-12-27 07:30:22 +0600

    chore: Add some help text

--- main.py
+++ main.py
@@ -3,9 +3,30 @@ from utils import color_print
 
 
 # # ask the user what they want
+greet = """
+Welcome back, this is handlebar your personal
+assistant for handling common task like 
+open websites or apps.
+to open sites like youtube write \"-o youtube\".
+to learn more about the features write \"--help".
+"""
+
+bicycle = """
+          __o
+  _ `-    _ \<_
+ ( O )----( O )
+"""
+color_print(bicycle)
+
+color_print(greet)
+
 user_input = ""
 
-while user_input != 'q':
-    color_print("What is it you want? ", end="")
-    user_input = input().lower()
-    app(user_input)
+try:
+    while user_input != 'q':
+        color_print("What is it you want? ", end="")
+        user_input = input().lower()
+        app(user_input)
+except:
+    print("\n\n")
+    color_print("You can write \"q\" to exit.")
