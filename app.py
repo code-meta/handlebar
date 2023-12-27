9602e644fb485d6b4c3a77f63a2338be49fe7c3d
fahim <fahim.x@yahoo.com>
2023-12-27 07:57:16 +0600

    chore: Update the help text
    This commit updates the help text, Additionally tweak
    the -o flag to -os.

--- app.py
+++ app.py
@@ -5,6 +5,30 @@ from utils import open_browser, color_print
 
 user_input = ""
 
+help_text = """
+Learn to use the followings in order use hanlebar better
+
+open sites with:
+    -os site_name 
+        Example: -os youtube
+        default supported sites are:
+            - youtube.com 
+            - imdb.com 
+            - google.com   
+            - yahoo.com   
+            - healthline.com    
+            - developer.mozilla.org 
+
+        for other sites you can use the following:
+            -osm site_domain
+            Example: -osm nytimes.com/international
+        
+        to open sites in a private tab use the following:
+            -os site_name -p 
+                Example: -os youtube -p
+            -osm site_domain -p
+                Example: -osm nytimes.com/international -p
+"""
 
 def open_world():
     if "youtube" in user_input:
@@ -17,11 +41,18 @@ def open_world():
         pyautogui.press("enter")
 
 
+def show_help():
+    color_print(help_text)
+
+
 def app(u_input):
     global user_input
-    user_input = u_input
+    user_input = str(u_input).lower()
 
-    if "-o" in user_input:
+    if "--help" in user_input:
+        show_help()
+
+    elif "-os" in user_input:
         open_world()
     else:
         color_print("Miss typed?, to learn more write --help")
